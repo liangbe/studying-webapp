67dd0dfcad69ec3399e0e9f395ce9fcb013940e3
liangbe <bee.liang@oocl.com>
2016-08-24 19:05:21 +0800

    jpa+hibernate

--- src/main/java/com/bee/test/controller/testController.java
+++ src/main/java/com/bee/test/controller/testController.java
@@ -1,21 +1,12 @@
 package com.bee.test.controller;
 
-import com.bee.test.entity.Person;
-import com.mchange.v2.c3p0.DriverManagerDataSource;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 /**
  * Created by LIANGBE on 8/11/2016.
@@ -31,17 +22,17 @@ public class testController  {
 
     @RequestMapping(value = "/myController", method = RequestMethod.GET)
     public ModelAndView  showPerson(@RequestParam String name){
-        Person p =new Person();
-        if(name.equals("liangbe")){
-            p.setAge(25);
-            p.setName(name);
-            p.setSex("female");
-        }else{
-            p.setAge(0);
-            p.setName("system not found !");
-            p.setSex(null);
-        }
-        return new ModelAndView("index","person",p);
+//        Person p =new Person();
+//        if(name.equals("liangbe")){
+//            p.setAge(25);
+//            p.setName(name);
+//            p.setSex("female");
+//        }else{
+//            p.setAge(0);
+//            p.setName("system not found !");
+//            p.setSex(null);
+//        }
+        return new ModelAndView("index");
     }
 
 
